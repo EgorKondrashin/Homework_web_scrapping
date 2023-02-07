9527c806f5e6d893225985271ce8e085f368f3d3
Egor Kondrashin <kondrashin-egor@mail.ru>
2023-02-08 00:19:24 +0500

    Revision

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import time
 
-from fake_useragent import UserAgent
 import requests
 from bs4 import BeautifulSoup
 import json
 
-ua = UserAgent()
+headers = {'Cookie': '_ym_uid=1639148487334283574; _ym_d=1639149414; _ga=GA1.2.528119004.1639149415; _gid=GA1.2.512914915.1639149415; habr_web_home=ARTICLES_LIST_ALL; hl=ru; fl=ru; _ym_isad=2; __gads=ID=87f529752d2e0de1-221b467103cd00b7:T=1639149409:S=ALNI_MYKvHcaV4SWfZmCb3_wXDx2olu6kw',
+           'Accept-Language': 'ru-RU,ru;q=0.9',
+           'Sec-Fetch-Dest': 'document',
+           'Sec-Fetch-Mode': 'navigate',
+           'Sec-Fetch-Site': 'same-origin',
+           'Sec-Fetch-User': '?1',
+           'Cache-Control': 'max-age=0',
+           'If-None-Match': 'W/"37433-+qZyNZhUgblOQJvD5vdmtE4BN6w"',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+           'sec-ch-ua-mobile': '?0'}
 data = []
 
 
@@ -13,7 +21,7 @@ def get_source_html():
     for i in range(0, 40):
         response = requests.get(
             url=f"https://spb.hh.ru/search/vacancy?text=python&area=1&area=2&page={i}",
-            headers={"user-agent": f"{ua.random}"}
+            headers=headers
         )
         soup = BeautifulSoup(response.text, "lxml")
         source_list = soup.find_all("div", class_="serp-item")
